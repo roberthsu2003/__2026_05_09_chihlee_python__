4c8a0b53ce82e1f6cd688d6589a891c7a87c30fb
徐國堂 <roberthsu2003@gmail.com>
2026-05-30 16:15:15 +0800

    modify

--- 05_30/lesson8.py
+++ 05_30/lesson8.py
@@ -11,3 +11,11 @@ with open("student.txt","r",encoding="utf-8") as file:
 	content = file.read()
 
 print(file.closed)
+
+#==============
+import csv
+
+with open("考試分數_3年6班.csv", "r", encoding="utf-8") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        print(row)
